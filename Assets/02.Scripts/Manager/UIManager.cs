93b4261cf1a4210f803c79bc62f857cea2ac8e5b
myeoungeun <melee0110@naver.com>
2025-08-18 21:05:05 +0900

    Feat : ui 기본틀 제작

--- Assets/02.Scripts/Manager/UIManager.cs
+++ Assets/02.Scripts/Manager/UIManager.cs
@@ -1,18 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
     {
-        
-    }
+        public GameObject menuPannel;
+        public GameObject gameOverPannel;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        public void Stop()
+        {
+            Time.timeScale = 0; //시간 0으로 해서 멈춤
+            menuPannel.SetActive(true); //옵션창 띄우기
+        }
+
+        public void ReStart()
+        {
+            menuPannel.SetActive(false);
+            Time.timeScale = 1f;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); //현재 씬 다시불러오기
+        }
+
+        public void Continue()
+        {
+            menuPannel.SetActive(false);
+            Time.timeScale = 1f;
+        }
+
+        public void Exit()
+        {
+            Debug.Log("나가기 기능");
+            UnityEditor.EditorApplication.isPlaying = false;
+            Application.Quit();
+        }
+
+        public void GameOver()
+        {
+            Debug.Log("게임오버");
+            gameOverPannel.SetActive(true);
+        }
     }
-}
