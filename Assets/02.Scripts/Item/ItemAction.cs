bc7c58f34b728e16e0796363e3fa15cb7c2b78ce
taewooo513 <taewooo030513@gmail.com>
2025-08-18 16:01:20 +0900

    feat: itemAction

--- Assets/02.Scripts/Item/ItemAction.cs
+++ Assets/02.Scripts/Item/ItemAction.cs
@@ -4,20 +4,19 @@ using UnityEngine;
 
 public partial class Item : MonoBehaviour // 여기서 버프사용시 함수만 
 {
-
     public void GetSpeedItem()
     {
-
+        GameManager.Instance.GetSpeedUpItem(val, maxTime);
     }
 
     public void GetInvItem()
     {
-
+        GameManager.Instance.GetInvItem(maxTime);
     }
 
     public void GetCoinItem()
     {
-
+        GameManager.Instance.AddScore((int)val);
     }
 
     public void GetTrophyItem()
