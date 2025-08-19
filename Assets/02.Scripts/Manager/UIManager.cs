b030e6bc7f8fc36d44951b366606737b5f429e0b
Winebug <ydjoo0410@gmail.com>
2025-08-19 18:01:40 +0900

    feat: slide effect 추가
    feat: slide effect 추가

--- Assets/02.Scripts/Manager/UIManager.cs
+++ Assets/02.Scripts/Manager/UIManager.cs
@@ -5,56 +5,56 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
-    {
-        public GameObject menuPannel;
-        public GameObject gameOverPannel;
-        public Image[] heartImages; // 하트(체력) 변수
+{
+    public GameObject menuPannel;
+    public GameObject gameOverPannel;
+    public Image[] heartImages; // 하트(체력) 변수
 
     public void Stop()
-        {
-            Time.timeScale = 0; //시간 0으로 해서 멈춤
-            menuPannel.SetActive(true); //옵션창 띄우기
-        }
+    {
+        Time.timeScale = 0; //시간 0으로 해서 멈춤
+        menuPannel.SetActive(true); //옵션창 띄우기
+    }
 
-        public void ReStart()
-        {
-            menuPannel.SetActive(false);
-            Time.timeScale = 1f;
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); //현재 씬 다시불러오기
-        }
+    public void ReStart()
+    {
+        menuPannel.SetActive(false);
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); //현재 씬 다시불러오기
+    }
 
-        public void Continue()
-        {
-            menuPannel.SetActive(false);
-            Time.timeScale = 1f;
-        }
+    public void Continue()
+    {
+        menuPannel.SetActive(false);
+        Time.timeScale = 1f;
+    }
 
-        public void Exit()
-        {
-            Debug.Log("나가기 기능");
-            UnityEditor.EditorApplication.isPlaying = false;
-            Application.Quit();
-        }
+    public void Exit()
+    {
+        Debug.Log("나가기 기능");
+        UnityEditor.EditorApplication.isPlaying = false;
+        Application.Quit();
+    }
 
-        public void GameOver()
-        {
-            Debug.Log("게임오버");
-            gameOverPannel.SetActive(true);
-        }
+    public void GameOver()
+    {
+        Debug.Log("게임오버");
+        gameOverPannel.SetActive(true);
+    }
 
-        public void UpdateHealthUI(int currentHealth)
+    public void UpdateHealthUI(int currentHealth)
+    {
+        for (int i = 0; i < heartImages.Length; i++)
         {
-            for (int i = 0; i < heartImages.Length; i++)
-                {
             // 현재 체력보다 인덱스가 크면 (잃어버린 체력 칸이면) 하트를 끈다
-                    if (i < currentHealth)
-                        {
-                            heartImages[i].enabled = true;
-                        }
-                    else
-                        {
-                            heartImages[i].enabled = false;
-                        }
-                }
+            if (i < currentHealth)
+            {
+                heartImages[i].enabled = true;
+            }
+            else
+            {
+                heartImages[i].enabled = false;
+            }
         }
+    }
 }
