f313f5d4bdbc0b36c2bc92a3de818677194d63cf
ChanyTheAwesome <89036528+ChanyTheAwesome@users.noreply.github.com>
2025-08-19 20:39:07 +0900

    [feat] achievement, still working on it

---
+++ Assets/02.Scripts/Achievement/AchievementData.cs
@@ -0,0 +1,16 @@
+public enum AchievementType
+{
+    Score,
+    Distance,
+    Time
+}
+
+public class AchievementData
+{
+    public int ID { get; set; }
+    public string AchievementName { get; set; }
+    public string Description { get; set; }
+    public AchievementType Type { get; set; }
+    public int GoalValue { get; set; }
+    public bool IsCleared { get; set; }
+}
