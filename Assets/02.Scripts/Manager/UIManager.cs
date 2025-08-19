037a93ad5f2dc6b0f278a30194e3739ec2ceb010
Winebug <ydjoo0410@gmail.com>
2025-08-19 11:35:50 +0900

    chore: chore
    chore: chore

--- Assets/02.Scripts/Manager/UIManager.cs
+++ Assets/02.Scripts/Manager/UIManager.cs
@@ -1,18 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // 인스펙터 창에서 3개의 하트 이미지를 연결할 배열
+    public Image[] heartImages;
+    public void UpdateHealthUI(int currentHealth)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        for (int i = 0; i < heartImages.Length; i++)
+        {
+            if (i < currentHealth)
+            {
+                heartImages[i].enabled = true; // 하트 이미지를 켠다
+            }
+            // 그렇지 않다면 (잃어버린 체력에 해당하는 하트라면)
+            else
+            {
+                heartImages[i].enabled = false; // 하트 이미지를 끈다
+            }
+        }
     }
 }
--- Assets/02.Scripts/Player/Player.cs
+++ Assets/02.Scripts/Player/Player.cs
@@ -2,15 +2,41 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private int maxHealth = 3;
+    private int currentHealth;
+
+    public UIManager uiManager;
+
+
     void Start()
     {
-        
+        currentHealth = maxHealth;
+
+        if (uiManager != null)
+        {
+            uiManager.UpdateHealthUI(currentHealth);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void TakeDamage(int damageAmount)
     {
-        
+        currentHealth -= damageAmount;
+        if (currentHealth < 0)
+        {
+            currentHealth = 0;
+        }
+
+        if (uiManager != null)
+        {
+            uiManager.UpdateHealthUI(currentHealth);
+        }
+        //========================================================
+
+        Debug.Log("데미지를 입었습니다! 현재 체력: " + currentHealth);
+
+        if (currentHealth <= 0)
+        {
+            //GameOver();
+        }
     }
 }
