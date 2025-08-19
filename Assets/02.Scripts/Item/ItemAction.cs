18179c018f5af9075cc1dd81cda9d6cf2df67a2f
ChanyTheAwesome <89036528+ChanyTheAwesome@users.noreply.github.com>
2025-08-19 21:24:59 +0900

    [fix] component bugs

--- Assets/02.Scripts/Item/ItemAction.cs
+++ Assets/02.Scripts/Item/ItemAction.cs
@@ -16,6 +16,7 @@ public partial class Item : MonoBehaviour // 여기서 버프사용시 함수만
 
     public void GetCoinItem()
     {
+        AudioManager.Instance.PlayCoinSound();
         GameManager.Instance.AddScore((int)val);
     }
 
