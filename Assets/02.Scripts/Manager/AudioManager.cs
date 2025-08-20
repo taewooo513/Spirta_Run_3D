c38e13c9877667f41e62f61a78bd75490ca97e76
taewooo513 <taewooo030513@gmail.com>
2025-08-20 12:54:22 +0900

    ref : 사운드 매니저 구조변경

--- Assets/02.Scripts/Manager/AudioManager.cs
+++ Assets/02.Scripts/Manager/AudioManager.cs
@@ -1,52 +1,120 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+
+public enum SoundKey
+{
+    eTestBGM,
+    eCarCrashA,
+    eCarCrashB,
+    eCarCrashC,
+    eCoin,
+    SoundCount,
+    SoundErr
+}
 
 public class AudioManager : Singleton<AudioManager>
 {
-    [SerializeField] private AudioClip coinSound;
-    [SerializeField] private AudioClip[] carCrashSound;
-    private AudioSource audioSource;
+    [SerializeField] private AudioClip coinSound; // 사용 X
+    [SerializeField] private AudioClip[] carCrashSound; // 수정전코드 
+    private AudioSource audioSource;  // BGM오디오 소스
+    private AudioSource audioEFXSource;  // EFX오디오 소스
+    [SerializeField]
+    private AudioClip[] sounds;
+    [SerializeField]
+    private SoundKey[] soundsKey;
+
 
     private void Start()
-    { 
-        audioSource = GetComponent<AudioSource>();
-        if(audioSource == null)
-        {
-            this.gameObject.AddComponent<AudioSource>();
-            audioSource = GetComponent<AudioSource>();
-        }
-    }
-    public void PlayCoinSound()
     {
-        if (coinSound != null)
+        var soundSources = transform.GetComponents<AudioSource>();
+        if (soundSources.Length != 2)
         {
-            audioSource?.PlayOneShot(coinSound);
+            audioSource = gameObject.AddComponent<AudioSource>();
+            audioEFXSource = gameObject.AddComponent<AudioSource>();
         }
         else
         {
-            Debug.LogWarning("Coin sound not assigned in AudioManager.");
+            audioSource = soundSources[0];
+            audioEFXSource = soundSources[0];
         }
+
+        audioSource.loop = true;
+
+        PlayLoopSound(SoundKey.eTestBGM);
+    }
+
+    public void PlaySoundEffect(SoundKey key)
+    {
+        int index = (int)key;
+        if (index < sounds.Length)
+        {
+            if (sounds[index] == null)
+            {
+                Debug.LogWarning("Coin sound not assigned in AudioManager.");
+            }
+            else
+            {
+                audioEFXSource.PlayOneShot(sounds[index]);
+            }
+        }
+        else
+        {
+            Debug.LogWarning($"{key}is out of range to  sounds {sounds.Length}.");
+        }
+    }
+
+    public void PlayLoopSound(SoundKey key)
+    {
+        int index = (int)key;
+        if (index < sounds.Length)
+        {
+            if (sounds[index] == null)
+            {
+                Debug.LogWarning("Coin sound not assigned in AudioManager.");
+            }
+            else
+            {
+                audioSource.clip = sounds[index];
+                audioSource.Play();
+                Debug.Log("fasd");
+            }
+        }
+        else
+        {
+            Debug.LogWarning($"{key}is out of range to  sounds {sounds.Length}.");
+        }
+    }
+    public void StopLoopSound()
+    {
+        audioSource.Stop();
+    }
+    public void StopEFXSound()
+    {
+        audioEFXSource.Stop();
+    }
+
+    public void PlayCoinSound()
+    {
+        PlaySoundEffect(SoundKey.eCoin);
     }
     public void PlayCarCrashSound()
     {
-        if (carCrashSound?.Length > 0)
-        {
-            audioSource?.PlayOneShot(GetRandomCarCrashSound());
-        }
-        else
-        {
-            Debug.LogWarning("Car crash sounds not assigned in AudioManager.");
-        }
+        PlaySoundEffect(RandomToSoundKey(SoundKey.eCarCrashA, SoundKey.eCarCrashC));
     }
-    private AudioClip GetRandomCarCrashSound()
+
+
+    private SoundKey RandomToSoundKey(SoundKey minNumber, SoundKey maxNumber)
     {
-        if (carCrashSound.Length == 0)
+        int val = int.Parse(maxNumber.ToString()) - int.Parse(minNumber.ToString());
+        if (val < 0)
         {
-            Debug.LogWarning("No car crash sounds available.");
-            return null;
+            Debug.LogError("maxNumber < minNumber");
+            return SoundKey.SoundErr;
         }
-        int randomIndex = Random.Range(0, carCrashSound.Length);
-        return carCrashSound[randomIndex];
+
+        return (SoundKey)UnityEngine.Random.Range(0, val);
     }
 }
