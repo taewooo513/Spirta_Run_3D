b4802dcd621afb20684c98d27eb84c764a742a5e
ChanyTheAwesome <89036528+ChanyTheAwesome@users.noreply.github.com>
2025-08-20 12:45:55 +0900

    [feat] public properties, InitializeScore()

--- Assets/02.Scripts/UI/Score.cs
+++ Assets/02.Scripts/UI/Score.cs
@@ -4,20 +4,34 @@ using UnityEngine;
 
 public class Score : MonoBehaviour
 {
-    private int _score;
+    private int _curScore;
+    public int CurScore
+    {
+        get { return _curScore; }
+    } //현재 점수
+
     private int _topScore;
+    public int TopScore
+    {
+        get { return _topScore; }
+    } //최고 점수
 
     public void AddScore(int val) //점수 추가
     {
-        _score += val;
-        TopScore();
+        _curScore += val;
+        SetTopScore();
     }
 
-    public void TopScore() //최고점수 갱신
+    public void SetTopScore() //최고점수 갱신
     {
-        if (_score > _topScore)
+        if (_curScore > _topScore)
         {
-          _topScore = _score;  
+            _topScore = _curScore;
         }
+    }
+
+    public void InitializeScore()
+    {
+        _curScore = 0;
     }
 }
