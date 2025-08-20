58799c2c1cc912896524ef14be8d316c831c49f9
ChanyTheAwesome <89036528+ChanyTheAwesome@users.noreply.github.com>
2025-08-20 13:06:43 +0900

    [chore]

--- Assets/02.Scripts/Item/ItemAction.cs
+++ Assets/02.Scripts/Item/ItemAction.cs
@@ -17,7 +17,7 @@ public partial class Item : MonoBehaviour // 여기서 버프사용시 함수만
 
     public void GetCoinItem()
     {
-        AudioManager.Instance.PlayCoinSound();
+        //AudioManager.Instance.PlayCoinSound();
         GameManager.Instance.score.AddScore((int)val);
         Debug.Log("코인 획득: " + val);
         Destroy(gameObject);
