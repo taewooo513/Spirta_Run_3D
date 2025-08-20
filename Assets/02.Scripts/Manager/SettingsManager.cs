4b7721708d1678d427293773338745c27d340157
Winebug <140414847+Winebug@users.noreply.github.com>
2025-08-20 19:10:46 +0900

    chore: chore
    chore: chore

---
+++ Assets/02.Scripts/Manager/SettingsManager.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SettingsManager : MonoBehaviour
+{
+    public static SettingsManager Instance { get; private set; }
+
+    public AudioMixer audioMixer;
+
+    private const string ControlTypeKey = "ControlType";
+    private const string BGMVolumeKey = "BGMVolume";
+    private const string SFXVolumeKey = "SFXVolume";
+
+    public enum ControlType { Tilt, Button }
+    public ControlType currentControlType;
+
+    // 게임이 시작될 때 가장 먼저 호출되는 함수
+    void Awake()
+    {
+        // 싱글턴 패턴 설정
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        LoadSettings();
+    }
+
+    private void LoadSettings()
+    {
+        currentControlType = (ControlType)PlayerPrefs.GetInt(ControlTypeKey, 0);
+
+        SetBGMVolume(PlayerPrefs.GetFloat(BGMVolumeKey, 1f));
+        SetSFXVolume(PlayerPrefs.GetFloat(SFXVolumeKey, 1f));
+    }
+    public void SetControlType(int controlIndex)
+    {
+        currentControlType = (ControlType)controlIndex;
+        PlayerPrefs.SetInt(ControlTypeKey, controlIndex);
+    }
+
+    // BGM 볼륨을 변경하고 저장하는 함수
+    public void SetBGMVolume(float volume)
+    {
+        audioMixer.SetFloat("BGMVolume", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat(BGMVolumeKey, volume);
+    }
+
+    public void SetSFXVolume(float volume)
+    {
+        audioMixer.SetFloat("SFXVolume", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat(SFXVolumeKey, volume);
+    }
+}
