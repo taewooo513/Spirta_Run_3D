04b57de242f5749d5ab7c9debcf61d815f8a6698
Winebug <ydjoo0410@gmail.com>
2025-08-21 20:49:44 +0900

    chore: 씬
    chore: 씬

--- Assets/02.Scripts/Item/Item.cs
+++ Assets/02.Scripts/Item/Item.cs
@@ -1,25 +1,34 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.Video;
 
 public partial class Item : MonoBehaviour
 {
-    public UnityEvent useItemAction; //아이템 사용시 호출하는 함수
+    public UnityEvent useItemAction; // 아이템 사용 시 호출하는 함수
 
     public float maxTime; // 버프지속시간
     public float val; // 수치 ex). 이속증가 얼마나되는지등
 
+    [Header("Effects")]
+    public GameObject collectionEffectPrefab; 
+    public VideoClip videoToPlay; 
+
     public void GetItem()
     {
-        Debug.Log("teaw");
+        // 이펙트 생성
+        if (collectionEffectPrefab != null)
+        {
+            Instantiate(collectionEffectPrefab, transform.position, Quaternion.identity);
+        }
+
+        // 비디오 재생 요청
+        if (videoToPlay != null)
+        {
+            VideoManager.Instance.PlayVideo(videoToPlay);
+        }
+
         useItemAction?.Invoke();
-        Debug.Log("teaw");
+
         Destroy(gameObject);
-    }
-    private void OnCollisionEnter(Collision collision)
-    {
-        transform.GetComponent<Rigidbody>().isKinematic = true;
-        transform.GetComponent<CapsuleCollider>().isTrigger = true;
     }
 }
