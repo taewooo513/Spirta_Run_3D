25c768590095544167e065718e3eb62d7252cea8
taewooo513 <taewooo030513@gmail.com>
2025-08-21 20:44:52 +0900

    버그수정

--- Assets/02.Scripts/MainGame.cs
+++ Assets/02.Scripts/MainGame.cs
@@ -5,12 +5,18 @@ using UnityEngine;
 public class MainGame : MonoBehaviour
 {
     // Start is called before the first frame update 구조를 잘못짜서 세팅여기서해주겠습니다
+    public GameObject airPlain;
     void Start()
     {
         PatternManager.Instance.StartGame();
         GameManager.Instance.GameStart();
+        Invoke("AirAdd", 10);
     }
 
+    void AirAdd()
+    {
+        Instantiate(airPlain, new Vector3(0, 13, 0), Quaternion.identity);
+    }
     // Update is called once per frame
     void Update()
     {
