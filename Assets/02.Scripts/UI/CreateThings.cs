cb329da57d1c7748e84b63a9999a9cc9dddb15a4
ChanyTheAwesome <89036528+ChanyTheAwesome@users.noreply.github.com>
2025-08-21 19:53:05 +0900

    [feat] Add Unity_Chan to StartScene

--- Assets/02.Scripts/UI/CreateThings.cs
+++ Assets/02.Scripts/UI/CreateThings.cs
@@ -12,7 +12,9 @@ public class CreateThings : MonoBehaviour
 
     private void CreateThing()
     {
+        float random = Random.Range(0.2f, 1.0f);
         GameObject thing = Instantiate(thingPrefab[Random.Range(0, thingPrefab.Length)], this.transform.position, Random.rotation);
+        thing.transform.localScale = new Vector3(random, random, random);
         Rigidbody rb = thing.AddComponent<Rigidbody>();
         rb.mass = 0.1f;
         rb.drag = 2.0f;
--- Assets/02.Scripts/UI/FontSizeChanger.cs
+++ Assets/02.Scripts/UI/FontSizeChanger.cs
@@ -6,7 +6,11 @@ using UnityEngine;
 public class FontSizeChanger : MonoBehaviour
 {
     private TextMeshProUGUI text;
+    [SerializeField] private float minFontSize;
+    [SerializeField] private float maxFontSize;
+    [SerializeField] private float startTime;
     private bool isIncreasing = true;
+    private float currentTime;
     private void Start()
     {
         text = GetComponent<TextMeshProUGUI>();
@@ -14,11 +18,16 @@ public class FontSizeChanger : MonoBehaviour
 
     private void Update()
     {
-        if(text.fontSize >= 100)
+        if(currentTime < startTime)
+        {
+            currentTime += Time.deltaTime;
+            return; // Wait until the start time has passed
+        }
+        if (text.fontSize >= maxFontSize)
         {
             isIncreasing = false;
         }
-        else if (text.fontSize <= 50)
+        else if (text.fontSize <= minFontSize)
         {
             isIncreasing = true;
         }
