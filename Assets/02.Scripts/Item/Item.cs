1d7d2335bf22621bcdef121c8af830e867f5183c
taewooo513 <taewooo030513@gmail.com>
2025-08-21 21:30:49 +0900

    item버그수정

--- Assets/02.Scripts/Item/Item.cs
+++ Assets/02.Scripts/Item/Item.cs
@@ -8,7 +8,7 @@ public partial class Item : MonoBehaviour
     public float val;     // 효과 수치 (점수, 속도 배율 등)
 
     [Header("Effects")]
-    public GameObject collectionEffectPrefab;        
+    public GameObject collectionEffectPrefab;
 
     [Header("Actions")]
     public UnityEvent useItemAction;
@@ -21,7 +21,11 @@ public partial class Item : MonoBehaviour
         }
         // 인스펙터에 연결된 추가 기능
         useItemAction?.Invoke();
+    }
 
-        Destroy(gameObject);
+    private void OnCollisionEnter(Collision collision)
+    {
+        GetComponent<Rigidbody>().isKinematic = true;
+        GetComponent<Collider>().isTrigger = true;
     }
 }
